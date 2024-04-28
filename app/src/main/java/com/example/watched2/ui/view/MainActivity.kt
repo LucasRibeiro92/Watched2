513302ca9bb5d817ed67b06e78d6794cc6d931c1
LucasRibeiro92 <lucasc.ribeiroo@gmail.com>
2024-04-27 23:53:03 -0300

    MainActivity initial implementation, with extra configs to make the comm with api works.

--- app/src/main/java/com/example/watched2/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/watched2/ui/view/MainActivity.kt
@@ -1,21 +1,43 @@
 package com.example.watched2.ui.view
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.watched2.R
+import com.example.watched2.databinding.ActivityMainBinding
+import com.example.watched2.ui.viewmodel.MovieViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var movieSearchEditText: EditText
+    private lateinit var movieSearchButton: Button
+
+    private lateinit var movieViewModel: MovieViewModel
+    private val TAG = "CHECK_RESPONSE"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        setupBindigs()
+        movieViewModel = MovieViewModel()
+        listenSearchButton()
+    }
+
+    private fun setupBindigs() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        movieSearchEditText = binding.movieSearchEditText
+        movieSearchButton = binding.movieSearchButton
+    }
+    private fun listenSearchButton() {
+        movieSearchButton.setOnClickListener{
+            val textToSearch = movieSearchEditText.text
+            movieViewModel.fetchMovieBySearch(textToSearch)
         }
     }
 }
